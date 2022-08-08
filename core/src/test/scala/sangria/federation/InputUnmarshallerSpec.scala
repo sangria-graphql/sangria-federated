74d3b6a4530c4996fe3e5907ad969bab6b1a8307
xsoufiane <soufiane.maguerra@commercetools.de>
2022-08-08 23:17:44 +0200

    test: rewrite InputUnmarshallerSpec to be more human readable

--- core/src/test/scala/sangria/federation/InputUnmarshallerSpec.scala
+++ core/src/test/scala/sangria/federation/InputUnmarshallerSpec.scala
@@ -1,82 +1,15 @@
 package sangria.federation
 
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
 import sangria.marshalling.InputUnmarshaller
 
-class InputUnmarshallerSpec extends AnyFreeSpec with Matchers with MockFactory {
+class InputUnmarshallerSpec extends AnyWordSpec with Matchers with MockFactory {
 
-  "Federated Upgraded InputUnmarshaller" - {
-    "MapNode Scalar" - {
-      "with __typeName is NodeObject" in {
-        val default = stub[InputUnmarshaller[Unit]]
-        (default.isMapNode _).when(()).returns(true)
-        (default.getMapValue _).when((), "__typename").returns(Some(()))
-
-        val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
-
-        upgrade.getScalarValue(()) shouldBe a[NodeObject[Unit]]
-      }
-      "without __typename" - {
-        "with a non-string name calls default getScalarValue" in {
-          val default = stub[InputUnmarshaller[Unit]]
-          (default.isMapNode _).when(()).returns(true)
-          (default.getMapValue _).when((), "__typename").returns(None)
-          (default.getMapValue _).when((), "name").returning(Some(()))
-          (default.getScalaScalarValue _).when(()).returns(())
-
-          val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
-          upgrade.getScalarValue(())
-
-          (default.getScalarValue _).verify(())
-        }
-        "with a string name" - {
-          "without as should be a Link__Import_Object with only name" in {
-            val default = stub[InputUnmarshaller[Unit]]
-            (default.isMapNode _).when(()).returns(true)
-            (default.getMapValue _).when((), "__typename").returns(None)
-            (default.getMapValue _).when((), "name").returning(Some(()))
-            (default.getScalaScalarValue _).when(()).returns("myname")
-            (default.getMapValue _).when((), "as").returns(None)
-
-            val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
-
-            upgrade.getScalarValue(()) should be(Link__Import_Object("myname"))
-          }
-          "with as" - {
-            "if as is a non-string it should be a Link__Import_Object with only name" in {
-              val default = stub[InputUnmarshaller[Unit]]
-              (default.isMapNode _).when(()).returns(true)
-              (default.getMapValue _).when((), "__typename").returns(None)
-              (default.getMapValue _).when((), "name").returning(Some(()))
-              (default.getScalaScalarValue _).when(()).once().returns("myname")
-              (default.getMapValue _).when((), "as").returns(Some(()))
-              (default.getScalaScalarValue _).when(()).once().returns(())
-
-              val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
-
-              upgrade.getScalarValue(()) should be(Link__Import_Object("myname"))
-            }
-            "if as is a string it should be a Link__Import_Object with name and as" in {
-              val default = stub[InputUnmarshaller[Unit]]
-              (default.isMapNode _).when(()).returns(true)
-              (default.getMapValue _).when((), "__typename").returns(None)
-              (default.getMapValue _).when((), "name").returning(Some(()))
-              (default.getScalaScalarValue _).when(()).once().returns("myname")
-              (default.getMapValue _).when((), "as").returns(Some(()))
-              (default.getScalaScalarValue _).when(()).once().returns("myas")
-
-              val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
-
-              upgrade.getScalarValue(()) should be(Link__Import_Object("myname", Some("myas")))
-            }
-          }
-        }
-      }
-    }
-    "Not-MapNode calls default getScalarValue" in {
+  "A Scalar" when {
+    "The Scalar is not an Object should call the default getScalarValue" in {
       val default = stub[InputUnmarshaller[Unit]]
       (default.isMapNode _).when(()).returns(false)
 
@@ -84,6 +17,72 @@ class InputUnmarshallerSpec extends AnyFreeSpec with Matchers with MockFactory {
       upgrade.getScalarValue(())
 
       (default.getScalarValue _).verify(())
+    }
+
+    "The Scalar is an Object with a __typeName field should be a NodeObject" in {
+      val default = stub[InputUnmarshaller[Unit]]
+      (default.isMapNode _).when(()).returns(true)
+      (default.getMapValue _).when((), "__typename").returns(Some(()))
+
+      val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
+
+      upgrade.getScalarValue(()) shouldBe a[NodeObject[Unit]]
+    }
+
+    "A Scalar that is an Object without '__typename' but with a 'name' field" when {
+      "the 'name' is a non-string, the default getScalarValue should be called" in {
+        val default = stub[InputUnmarshaller[Unit]]
+        (default.isMapNode _).when(()).returns(true)
+        (default.getMapValue _).when((), "__typename").returns(None)
+        (default.getMapValue _).when((), "name").returning(Some(()))
+        (default.getScalaScalarValue _).when(()).returns(())
+
+        val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
+        upgrade.getScalarValue(())
+
+        (default.getScalarValue _).verify(())
+      }
+      "The 'name' is a String, the Scalar should be a Link__Import__Object" in {
+        val default = stub[InputUnmarshaller[Unit]]
+        (default.isMapNode _).when(()).returns(true)
+        (default.getMapValue _).when((), "__typename").returns(None)
+        (default.getMapValue _).when((), "name").returning(Some(()))
+        (default.getScalaScalarValue _).when(()).returns("myname")
+        (default.getMapValue _).when((), "as").returns(None)
+
+        val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
+
+        upgrade.getScalarValue(()) should be(Link__Import_Object("myname"))
+      }
+    }
+
+    "The Scalar is an Object without __typename but with a string 'name' field and an 'as' field" when {
+      "The 'as' is a non-string, the Scalar should be a Link__Inport__Object with only name" in {
+        val default = stub[InputUnmarshaller[Unit]]
+        (default.isMapNode _).when(()).returns(true)
+        (default.getMapValue _).when((), "__typename").returns(None)
+        (default.getMapValue _).when((), "name").returning(Some(()))
+        (default.getScalaScalarValue _).when(()).once().returns("myname")
+        (default.getMapValue _).when((), "as").returns(Some(()))
+        (default.getScalaScalarValue _).when(()).once().returns(())
+
+        val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
+
+        upgrade.getScalarValue(()) should be(Link__Import_Object("myname"))
+      }
+      "The 'as' is a String, the Scalar should be a Link__Inport__Object with name and as" in {
+        val default = stub[InputUnmarshaller[Unit]]
+        (default.isMapNode _).when(()).returns(true)
+        (default.getMapValue _).when((), "__typename").returns(None)
+        (default.getMapValue _).when((), "name").returning(Some(()))
+        (default.getScalaScalarValue _).when(()).once().returns("myname")
+        (default.getMapValue _).when((), "as").returns(Some(()))
+        (default.getScalaScalarValue _).when(()).once().returns("myas")
+
+        val upgrade: InputUnmarshaller[Unit] = Federation.upgrade(default)
+
+        upgrade.getScalarValue(()) should be(Link__Import_Object("myname", Some("myas")))
+      }
     }
   }
 }
