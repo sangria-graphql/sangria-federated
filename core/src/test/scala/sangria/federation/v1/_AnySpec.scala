961e52093c095ba5c51fe11cbb14bc61e8e737e3
xsoufiane <soufiane.maguerra@commercetools.de>
2022-08-18 14:20:04 +0200

    test: add _AnySpec

---
+++ core/src/test/scala/sangria/federation/v1/_AnySpec.scala
@@ -0,0 +1,24 @@
+package sangria.federation.v1
+
+import io.circe._
+import io.circe.parser._
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.EitherValues
+import sangria.validation.Violation
+
+class _AnySpec extends AnyWordSpec with Matchers with MockFactory with EitherValues {
+
+  implicit val um = Federation.upgrade(sangria.marshalling.circe.CirceInputUnmarshaller)
+
+  "_Any scalar coercion accepts an object with __typename field" in {
+    // https://www.apollographql.com/docs/federation/v1/federation-spec#scalar-_any
+    parseUserInput(
+      parse("""{ "__typename": "foo", "foo": "bar" }""")
+        .getOrElse(Json.Null)).value shouldBe a[_Any[Json]]
+  }
+
+  def parseUserInput(value: Json): Either[Violation, _Any[Json]] =
+    _Any.__type.coerceUserInput(um.getScalarValue(value))
+}
