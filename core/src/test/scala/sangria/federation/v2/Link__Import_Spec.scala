41c5430982c01a970511e5059cd726defe3ff9aa
xsoufiane <soufiane.maguerra@commercetools.de>
2022-08-18 13:51:34 +0200

    test: refine Link__Import_Spec to be more descriptive

--- core/src/test/scala/sangria/federation/v2/Link__Import_Spec.scala
+++ core/src/test/scala/sangria/federation/v2/Link__Import_Spec.scala
@@ -1,24 +1,44 @@
 package sangria.federation.v2
 
+import io.circe._
+import io.circe.parser._
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.EitherValues
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import sangria.validation.Violation
 
 class Link__Import_Spec extends AnyWordSpec with Matchers with MockFactory with EitherValues {
 
+  implicit val um = Federation.upgrade(sangria.marshalling.circe.CirceInputUnmarshaller)
+
   "Scalar Link__Import coercion (https://specs.apollo.dev/link/v1.0/#example-import-a-string-name)" should {
     "accept a string" in {
-      Link__Import.Type.coerceUserInput("str").value shouldBe a[Link__Import]
+      // https://specs.apollo.dev/link/v1.0/#example-import-a-string-name
+      parseUserInput(Json.fromString("@link")).value shouldBe a[Link__Import]
     }
-    "accept an object with 'name' and an optional 'as' string fields" in {
-      Link__Import.Type
-        .coerceUserInput(Link__Import_Object("str"))
-        .value shouldBe a[Link__Import_Object]
+    "accept an object with a 'name' string field" in {
+      // https://specs.apollo.dev/link/v1.0/#example-import-an-aliased-name
+      parseUserInput(
+        parse("""{ "name": "@example" }""").getOrElse(
+          Json.Null)).value shouldBe Link__Import_Object(name = "@example")
     }
-    "raise violation error in any other case" in {
-      Link__Import.Type.coerceUserInput(()).left.value should be(
+    "accept an object with 'name' and an optional 'as' string field" in {
+      // https://specs.apollo.dev/link/v1.0/#example-import-an-aliased-name
+      parseUserInput(
+        parse("""{ "name": "@example", "as": "@eg" }""").getOrElse(
+          Json.Null)).value shouldBe Link__Import_Object(name = "@example", Some("@eg"))
+    }
+    "raise exception if 'name' field is not a string" in {
+      an[IllegalStateException] should be thrownBy parseUserInput(
+        parse("""{ "name": 1 }""").getOrElse(Json.Null)).value
+    }
+    "raise violation error on any other scalar" in {
+      parseUserInput(Json.fromInt(1)).left.value should be(
         Link__Import.Link__Import_Coercion_Violation)
     }
   }
+
+  def parseUserInput(value: Json): Either[Violation, Abstract_Link__Import] =
+    Link__Import.Type.coerceUserInput(um.getScalarValue(value))
 }
