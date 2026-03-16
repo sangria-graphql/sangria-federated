94d3780886f871d5228c4c7c7fa3fd701b7cb312
Scala Steward <scala_steward@virtuslab.com>
2026-03-16 22:23:20 +0000

    Update weaver-cats to 0.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,5 +23,5 @@ object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
   val scalapbRuntime =
     "com.thesamet.scalapb" %% "scalapb-runtime" % scalapb.compiler.Version.scalapbVersion % "protobuf"
-  val weaver = "org.typelevel" %% "weaver-cats" % "0.11.3"
+  val weaver = "org.typelevel" %% "weaver-cats" % "0.12.0"
 }
